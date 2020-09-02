f7cfbabaf7bf6e6de49b5172638dd15785d74e45
tsukanov_roman <roman.tsukanov@devexpress.com>
2020-09-02 16:34:52 +0300

    Add code for React

---
+++ react/src/App.js
@@ -0,0 +1,139 @@
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.css';
+import React, { useState } from 'react';
+
+
+import {
+  DataGrid,
+  ColumnChooser,
+  Column,
+  RequiredRule,
+  FilterRow,
+  SearchPanel,
+  GroupPanel,
+  Selection,
+  Summary,
+  TotalItem,
+  Editing,
+  MasterDetail,
+  Export
+} from 'devextreme-react/data-grid';
+import { employees } from './employees';
+import { Workbook } from 'exceljs';
+import saveAs from 'file-saver';
+import { exportDataGrid } from 'devextreme/excel_exporter';
+
+function SelectedEmployee(props) {
+  if(props.employee) {
+    return (
+      <p id="selected-employee">
+        Selected employee: {props.employee.FullName}
+      </p>
+    );
+  }
+  return null;
+}
+
+function DetailSection(props) {
+  const employee = props.data.data;
+  return (
+    <div>
+      <img
+        className="employee-photo"
+        alt={employee.FullName}
+        src={employee.Photo}
+      />
+      <p className="employee-notes">{employee.Notes}</p>
+    </div>
+  );
+}
+
+function exportGrid(e) {
+  const workbook = new Workbook(); 
+  const worksheet = workbook.addWorksheet("Main sheet"); 
+  exportDataGrid({ 
+      worksheet: worksheet, 
+      component: e.component
+  }).then(function() {
+      workbook.xlsx.writeBuffer().then(function(buffer) { 
+          saveAs(new Blob([buffer], { type: "application/octet-stream" }), "DataGrid.xlsx"); 
+      }); 
+  });
+  e.cancel = true; 
+}
+
+function App() {
+  const [selectedEmployee, setSelectedEmployee] = useState();
+  const selectEmployee = (e) => {
+    e.component.byKey(e.currentSelectedRowKeys[0]).done(employee => {
+        setSelectedEmployee(employee);
+    });
+  }
+
+  return (
+    <div className="App">
+      <DataGrid
+        dataSource={employees}
+        keyExpr="EmployeeID"
+        allowColumnResizing={true}
+        columnAutoWidth={true}
+        allowColumnReordering={true}
+        onSelectionChanged={selectEmployee}
+        onExporting={exportGrid}>
+        <ColumnChooser enabled={true} />
+        <Column dataField="FullName">
+          <RequiredRule />
+        </Column>
+        <Column dataField="Position">
+          <RequiredRule />
+        </Column>
+        <Column
+          dataField="BirthDate"
+          dataType="date"
+          width={150}>
+          <RequiredRule />
+        </Column>
+        <Column
+          dataField="HireDate"
+          dataType="date"
+          width={150}>
+          <RequiredRule />
+        </Column>
+        <Column dataField="City" />
+        <Column
+          dataField="Country"
+          groupIndex={0}
+          sortOrder="asc">
+          <RequiredRule />
+        </Column>
+        <Column dataField="Address" />
+        <Column dataField="HomePhone" />
+        <Column dataField="PostalCode" visible={false} />
+        <FilterRow visible={true} />
+        <SearchPanel visible={true} />
+        <GroupPanel visible={true} />
+        <Selection mode="single" />
+        <Summary>
+          <TotalItem
+            summaryType="count"
+            showInColumn="HomePhone"
+          />
+        </Summary>
+        <Editing
+          mode="popup"
+          allowUpdating={true}
+          allowDeleting={true}
+          allowAdding={true}
+        />
+        <MasterDetail
+          enabled={true}
+          component={DetailSection}
+        />
+        <Export enabled={true} />
+      </DataGrid>
+      <SelectedEmployee employee={selectedEmployee} />
+    </div>
+  );
+}
+
+export default App;
