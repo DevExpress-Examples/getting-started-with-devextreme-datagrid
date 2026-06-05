7c442d6e86d90ebcc004c079be7bbf629b6ea0e1
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-05 17:15:26 +0300

    Vue - Add DevExtreme licensing

--- Vue/src/main.ts
+++ Vue/src/main.ts
@@ -1,3 +1,8 @@
+import config from 'devextreme/core/config';
+import { licenseKey } from './devextreme-license';
+
+config({ licenseKey });
+
 import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.css';
 import { createApp } from 'vue';
