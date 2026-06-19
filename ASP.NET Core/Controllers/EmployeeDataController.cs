9221fb8bff3ee5a60e9b970c3a858ba39624424c
Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
2026-06-19 15:09:46 +0400

    Add Copilot Suggested Fixes

--- ASP.NET Core/Controllers/EmployeeDataController.cs
+++ ASP.NET Core/Controllers/EmployeeDataController.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using ASP_NET_Core.Models;
 using DevExtreme.AspNet.Data;
 using DevExtreme.AspNet.Mvc;
@@ -10,8 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_NET_Core.Controllers;
 
-[Route("api/[controller]")]
-public class EmployeeDataController: Controller {
+public class EmployeeDataController : Controller {
 
     [HttpGet]
     public object Get(DataSourceLoadOptions loadOptions) {
